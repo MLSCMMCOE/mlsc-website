f5ea3f3cb059ce0d9a32fd374bee985cdd580337
Ayush Deshpande <93706892+asyncdoggo@users.noreply.github.com>
2023-08-31 09:50:13 +0000

    Add download button to admin

--- admin/admin.js
+++ admin/admin.js
@@ -2,6 +2,7 @@ const { createClient } = supabase
 
 let _supabase
 let adminAuthClient 
+let g_data
 
 document.getElementById("login").addEventListener("submit",(e)=>{
     e.preventDefault()
@@ -28,18 +29,10 @@ async function getAllUsers() {
 
     if(error){
         console.log(error)
+        document.getElementById("login").style.display = "flex"
         return
     }
-    console.log(data)
-
-    // data = {
-    //     "name":"sanket",
-    //     "email":"",
-    //     subject:"",
-    //     message:"",
-    //     created_at:""
-    // }
-
+    g_data = data
     let tableDiv = document.getElementById("response_table")
     tableDiv.innerHTML = `<table id="responses">
     <tr>
@@ -59,5 +52,38 @@ async function getAllUsers() {
         <td>${data[i].created_at}</td>
     </tr>`
     }
+    const button = document.createElement("button")
+    button.innerHTML = "Download"
+    button.id = "download"
+    button.onclick = download
+    button.style.margin = "10px"
+    button.style.padding = "10px"
+    button.style.borderRadius = "5px"
+    button.style.backgroundColor = "#000"
+    button.style.color = "#fff"
+    button.style.border = "none"
+    button.style.cursor = "pointer"
+    button.style.outline = "none"
+    
+    tableDiv.appendChild(button)
 }
 
+
+const download = ()=>{
+    let csvContent = "";
+    csvContent += "Name,Email,Subject,Message,Created At\n"
+    g_data.forEach(function(rowArray) {
+      let row = rowArray.name+","+rowArray.email+","+rowArray.subject+","+rowArray.message+","+rowArray.created_at+"\n";
+      csvContent += row;
+    });
+
+    var blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    var url = URL.createObjectURL(blob);
+  
+    // Create a link to download it
+    var pom = document.createElement('a');
+    pom.href = url;
+    pom.setAttribute('download', "export.csv");
+    pom.click();
+
+}
