79380defbe3cfe6f0a7d7b5e7d91adc0bd60b3b3
DarkSoul26 <sanchitkalsi@gmail.com>
2023-10-14 12:21:32 +0530

    add team members info

--- src/components/team_data.jsx
+++ src/components/team_data.jsx
@@ -1,168 +1,269 @@
 const team = {
-    "Core": [
-      {
-        name: "Sahil Sarkate",
-        role: "β - MLSA",
-        img: new URL("../assets/Team_Profiles/sahil_sarkate.png", import.meta.url).href,
-        socials: {
-          instagram: "https://www.instagram.com/_sxhillll_16/",
-          github: "https://github.com/Sahil-Sarkate",
-          linkedin: "https://www.linkedin.com/in/sahil-sarkate-86514b1b5/",
-        },
+  "Core": [
+    {
+      name: "Sahil Sarkate",
+      role: "β - MLSA",
+      img: new URL("../assets/Team_Profiles/sahil_sarkate.png", import.meta.url).href,
+      socials: {
+        instagram: "https://www.instagram.com/_sxhillll_16/",
+        github: "https://github.com/Sahil-Sarkate",
+        linkedin: "https://www.linkedin.com/in/sahil-sarkate-86514b1b5/",
       },
-      {
-        name: "Yuvraj Singh",
-        role: "AI-ML Head",
-        img: new URL("../assets/Team_Profiles/yuvraj_singh.png", import.meta.url).href,
-        socials: {
-          linktree: "https://linktr.ee/sukhmani1303",
-          github: "https://github.com/sukhmani1303",
-          linkedin: "https://www.linkedin.com/in/sukhmani1303",
-        },
+    },
+    {
+      name: "Yuvraj Singh",
+      role: "AI-ML Head",
+      img: new URL("../assets/Team_Profiles/yuvraj_singh.png", import.meta.url).href,
+      socials: {
+        linktree: "https://linktr.ee/sukhmani1303",
+        github: "https://github.com/sukhmani1303",
+        linkedin: "https://www.linkedin.com/in/sukhmani1303",
       },
-      {
-        name: "Om Thakkar",
-        role: "Development Head",
-        img: new URL("../assets/Team_Profiles/Om_Thakkar.png", import.meta.url).href,
-        socials: {
-          instagram: "https://instagram.com/omthakkar343",
-          github: "https://github.com/OmThakkar",
-          linkedin: "https://www.linkedin.com/in/om-thakkar-482b471b0",
-        },
+    },
+    {
+      name: "Om Thakkar",
+      role: "Development Head",
+      img: new URL("../assets/Team_Profiles/Om_Thakkar.png", import.meta.url).href,
+      socials: {
+        instagram: "https://instagram.com/omthakkar343",
+        github: "https://github.com/OmThakkar",
+        linkedin: "https://www.linkedin.com/in/om-thakkar-482b471b0",
       },
-      {
-        name: "Ayush Deshpande",
-        role: "Cloud Head",
-        img: new URL("../assets/Team_Profiles/Ayush_Deshpande.png", import.meta.url).href,
-        socials: {
-          instagram: "https://instagram.com/ayush.arc",
-          github: "https://github.com/asyndoggo",
-          linkedin: "https://linkedin.com/in/ayush-deshpande",
-        },
+    },
+    {
+      name: "Ayush Deshpande",
+      role: "Cloud Head",
+      img: new URL("../assets/Team_Profiles/Ayush_Deshpande.png", import.meta.url).href,
+      socials: {
+        instagram: "https://instagram.com/ayush.arc",
+        github: "https://github.com/asyndoggo",
+        linkedin: "https://linkedin.com/in/ayush-deshpande",
       },
-      {
-        name: "Vedant Dumbhare",
-        role: "Corporate Head",
-        img: new URL("../assets/Team_Profiles/Vedant_Dumbhare.png", import.meta.url).href,
-        socials: {
-          instagram: "https://www.instagram.com/_vspacebar20",
-          github: "https://github.com/vpanada20",
-          linkedin: "http://www.linkedin.com/in/vedant-dumbhare-421a12144",
-        },
+    },
+    {
+      name: "Vedant Dumbhare",
+      role: "Corporate Head",
+      img: new URL("../assets/Team_Profiles/Vedant_Dumbhare.png", import.meta.url).href,
+      socials: {
+        instagram: "https://www.instagram.com/_vspacebar20",
+        github: "https://github.com/vpanada20",
+        linkedin: "http://www.linkedin.com/in/vedant-dumbhare-421a12144",
       },
-      {
-        name: "Harshvardhan Grandhi",
-        role: "Project Manager",
-        img: new URL("../assets/Team_Profiles/HarshvardhanGrandhi.png", import.meta.url).href,
-        "socials": {
-          "instagram": "https://www.instagram.com/hvsgrandhi_0508/",
-          "github": "https://github.com/hvsgrandhi",
-          "linkedin": "https://www.linkedin.com/in/harshvardhangrandhi/"
-        }
-      },
-      {
-        name: "Jaydatta Patwe",
-        role: "Management Head",
-        img: new URL("../assets/Team_Profiles/Jaydatta_Patwe.png", import.meta.url).href,
-        socials: {
-          instagram: "https://instagram.com/_jaydatt._",
-          github: "https://github.com/jaypatwe",
-          linkedin: "https://www.linkedin.com/in/jaydatta-patwe-4ba9aa1b2",
-        }
+    },
+    {
+      name: "Harshvardhan Grandhi",
+      role: "Project Manager",
+      img: new URL("../assets/Team_Profiles/HarshvardhanGrandhi.png", import.meta.url).href,
+      "socials": {
+        "instagram": "https://www.instagram.com/hvsgrandhi_0508/",
+        "github": "https://github.com/hvsgrandhi",
+        "linkedin": "https://www.linkedin.com/in/harshvardhangrandhi/"
       }
-    ],
+    },
+    {
+      name: "Jaydatta Patwe",
+      role: "Management Head",
+      img: new URL("../assets/Team_Profiles/Jaydatta_Patwe.png", import.meta.url).href,
+      socials: {
+        instagram: "https://instagram.com/_jaydatt._",
+        github: "https://github.com/jaypatwe",
+        linkedin: "https://www.linkedin.com/in/jaydatta-patwe-4ba9aa1b2",
+      }
+    }
+  ],
 
-    "AI-ML": [
-      {
-        name: "Yuvraj Singh",
-        role: "AI-ML Head",
-        img: new URL("../assets/Team_Profiles/yuvraj_singh.png", import.meta.url).href,
-        socials: {
-          linktree: "https://linktr.ee/sukhmani1303",
-          github: "https://github.com/sukhmani1303",
-          linkedin: "https://www.linkedin.com/in/sukhmani1303",
-        }
-      },
-      {
-        name: "Swanand Kulkarni",
-        role: "Member",
-        img: new URL("../assets/Team_Profiles/Swanand_Kulkarni.png", import.meta.url).href,
-        socials: {
-          instagram: "https://www.instagram.com/swanand4902/",
-          github: "https://github.com/SSK007-b",
-          linkedin: "https://linkedin.com/in/SSK007",
-        }
-      },
-      {
-        name: "Mansi Prasad",
-        role: "Member",
-        img: new URL("../assets/Team_Profiles/Mansi_Prasad.png", import.meta.url).href,
-        socials: {
-          instagram: "https://instagram.com/_mansi_prasad",
-          github: "https://github.com/mansiprasad034/",
-          linkedin: "https://www.linkedin.com/in/mansi-prasad-6ba956245/",
-        }
-      },
-      {
-        name: "Aatish Jawalkar",
-        role: "Member",
-        img: new URL("../assets/Team_Profiles/Aatish_Jawalkar.png", import.meta.url).href,
-        socials: {
-          instagram: "https://instagram.com/aatish1804",
-          github: "https://github.com/aatishmj",
-          linkedin: "https://www.linkedin.com/in/aatish-jawalkar-919ab3240",
-        }
-      },
-    ],
-    "Development": [
-      {
-        name: "Om Thakkar",
-        role: "Development Head",
-        img: new URL("../assets/Team_Profiles/Om_Thakkar.png", import.meta.url).href,
-        socials: {
-          instagram: "https://instagram.com/omthakkar343",
-          github: "https://github.com/OmThakkar",
-          linkedin: "https://www.linkedin.com/in/om-thakkar-482b471b0",
-        },
+  "AI-ML": [
+    {
+      name: "Yuvraj Singh",
+      role: "AI-ML Head",
+      img: new URL("../assets/Team_Profiles/yuvraj_singh.png", import.meta.url).href,
+      socials: {
+        linktree: "https://linktr.ee/sukhmani1303",
+        github: "https://github.com/sukhmani1303",
+        linkedin: "https://www.linkedin.com/in/sukhmani1303",
       }
-    ],
-    "Cloud": [
-      {
-        name: "Ayush Deshpande",
-        role: "Cloud Head",
-        img: new URL("../assets/Team_Profiles/Ayush_Deshpande.png", import.meta.url).href,
-        socials: {
-          instagram: "https://instagram.com/ayush.arc",
-          github: "github.com/asyndoggo",
-          linkedin: "https://linkedin.com/in/ayush-deshpande",
-        },
+    },
+    {
+      name: "Swanand Kulkarni",
+      role: "Member",
+      img: new URL("../assets/Team_Profiles/Swanand_Kulkarni.png", import.meta.url).href,
+      socials: {
+        instagram: "https://www.instagram.com/swanand4902/",
+        github: "https://github.com/SSK007-b",
+        linkedin: "https://linkedin.com/in/SSK007",
       }
-    ],
-    "Corporate": [
-      {
-        name: "Vedant Dumbhare",
-        role: "Corporate Head",
-        img: new URL("../assets/Team_Profiles/Vedant_Dumbhare.png", import.meta.url).href,
-        socials: {
-          instagram: "https://www.instagram.com/_vspacebar20",
-          github: "https://github.com/vpanada20",
-          linkedin: "http://www.linkedin.com/in/vedant-dumbhare-421a12144",
-        },
+    },
+    {
+      name: "Mansi Prasad",
+      role: "Member",
+      img: new URL("../assets/Team_Profiles/Mansi_Prasad.png", import.meta.url).href,
+      socials: {
+        instagram: "https://instagram.com/_mansi_prasad",
+        github: "https://github.com/mansiprasad034/",
+        linkedin: "https://www.linkedin.com/in/mansi-prasad-6ba956245/",
       }
-    ],
-    "Management": [
-      {
-        name: "Jaydatta Patwe",
-        role: "Management Head",
-        img: new URL("../assets/Team_Profiles/Jaydatta_Patwe.png", import.meta.url).href,
-        socials: {
-          instagram: "https://instagram.com/_jaydatt._",
-          github: "https://github.com/jaypatwe",
-          linkedin: "https://www.linkedin.com/in/jaydatta-patwe-4ba9aa1b2",
-        }
+    },
+    {
+      name: "Aatish Jawalkar",
+      role: "Member",
+      img: new URL("../assets/Team_Profiles/Aatish_Jawalkar.png", import.meta.url).href,
+      socials: {
+        instagram: "https://instagram.com/aatish1804",
+        github: "https://github.com/aatishmj",
+        linkedin: "https://www.linkedin.com/in/aatish-jawalkar-919ab3240",
       }
-    ],
-  };
+    },
+  ],
+  "Development": [
+    {
+      name: "Om Thakkar",
+      role: "Development Head",
+      img: new URL("../assets/Team_Profiles/Om_Thakkar.png", import.meta.url).href,
+      socials: {
+        instagram: "https://instagram.com/omthakkar343",
+        github: "https://github.com/OmThakkar",
+        linkedin: "https://www.linkedin.com/in/om-thakkar-482b471b0",
+      },
+    },
+    {
+      name: "Suyash Kirdakar",
+      role: "Member",
+      img: new URL("../assets/Team_Profiles/Suyash_Kirdakar.png", import.meta.url).href,
+      socials: {
+        instagram: "https://instagram.com/suyash2k04",
+        github: "https://github.com/suyash2k04",
+        linkedin: "https://www.linkedin.com/in/suyash-kirdakar-794132252",
+      },
+    },
+    {
+      name: "Prajakta Gunjal",
+      role: "Member",
+      img: new URL("../assets/Team_Profiles/Prajakta gunjal.png", import.meta.url).href,
+      socials: {
+        instagram: "https://www.instagram.com/pra.j.9/",
+        github: "https://github.com/Prajaktaa93",
+        linkedin: "https://www.linkedin.com/in/prajakta-gunjal-57439b260/",
+      },
+    },
+    {
+      name: "Tanmay Kale",
+      role: "Member",
+      img: new URL("../assets/Team_Profiles/Tanmay_Kale.png", import.meta.url).href,
+      socials: {
+        instagram: "https://instagram.com/_tanmaykale__",
+        github: "https://github.com/tammy022002",
+        linkedin: "https://www.linkedin.com/in/tanmay-kale-a5a237154",
+      },
+    },
+    {
+      name: "Saksham Avasthi",
+      role: "Member",
+      img: new URL("../assets/Team_Profiles/Saksham_Avasthi.png", import.meta.url).href,
+      socials: {
+        instagram: "",
+        github: "https://github.com/dumaloo",
+        linkedin: "https://www.linkedin.com/in/om-thakkar-482b471b0",
+      },
+    }
+
+  ],
+  "Cloud": [
+    {
+      name: "Ayush Deshpande",
+      role: "Cloud Head",
+      img: new URL("../assets/Team_Profiles/Ayush_Deshpande.png", import.meta.url).href,
+      socials: {
+        instagram: "https://instagram.com/ayush.arc",
+        github: "github.com/asyndoggo",
+        linkedin: "https://linkedin.com/in/ayush-deshpande",
+      },
+    },
+    {
+      name: "Atharv Nalwade",
+      role: "Member",
+      img: new URL("../assets/Team_Profiles/AtharvNalwade_member.png", import.meta.url).href,
+      socials: {
+        instagram: "",
+        github: "https://github.com/Atharv-Nalwade",
+        linkedin: "https://www.linkedin.com/in/iatharvn",
+      },
+    }
+  ],
+  "Corporate": [
+    {
+      name: "Vedant Dumbhare",
+      role: "Corporate Head",
+      img: new URL("../assets/Team_Profiles/Vedant_Dumbhare.png", import.meta.url).href,
+      socials: {
+        instagram: "https://www.instagram.com/_vspacebar20",
+        github: "https://github.com/vpanada20",
+        linkedin: "http://www.linkedin.com/in/vedant-dumbhare-421a12144",
+      },
+    },
+    {
+      name: "Shraddha Deshmukh",
+      role: "Member",
+      img: new URL("../assets/Team_Profiles/Shraddha_Deshmukh(1).png", import.meta.url).href,
+      socials: {
+        instagram: "https://instagram.com/_shraddha_.d",
+        github: "https://github.com/deshmukhshraddha",
+        linkedin: "https://www.linkedin.com/in/shraddha-deshmukh-356864246",
+      },
+    },
+    {
+      name: "Ojasvi Pawar",
+      role: "Member",
+      img: new URL("../assets/Team_Profiles/Ojasvi_Pawar.png", import.meta.url).href,
+      socials: {
+        instagram: "https://instagram.com/_.ojasvii._",
+        github: "https://github.com/Ojasvip26",
+        linkedin: "https://www.linkedin.com/in/ojasvi-pawar-4aa6b8230",
+      },
+    }
+  ],
+  "Management": [
+    {
+      name: "Jaydatta Patwe",
+      role: "Management Head",
+      img: new URL("../assets/Team_Profiles/Jaydatta_Patwe.png", import.meta.url).href,
+      socials: {
+        instagram: "https://instagram.com/_jaydatt._",
+        github: "https://github.com/jaypatwe",
+        linkedin: "https://www.linkedin.com/in/jaydatta-patwe-4ba9aa1b2",
+      }
+    },
+    {
+      name: "Shweta Mangnale",
+      role: "Member",
+      img: new URL("../assets/Team_Profiles/Shweta.png", import.meta.url).href,
+      socials: {
+        instagram: "https://www.instagram.com/shwetaa_76/",
+        github: "https://github.com/Shwetuu76",
+        linkedin: "http://www.linkedin.com/in/shweta-mangnale-a64170253",
+      }
+    },
+    {
+      name: "Sanika Thite",
+      role: "Member",
+      img: new URL("../assets/Team_Profiles/Sanika_Thite.png", import.meta.url).href,
+      socials: {
+        instagram: "https://instagram.com/sanika_0209_",
+        github: "https://github.com/Sanikathite",
+        linkedin: "https://www.linkedin.com/in/sanika-thite-3bb560269",
+      }
+    },
+    {
+      name: "Arya Deshpande",
+      role: "Member",
+      img: new URL("../assets/Team_Profiles/Aarya.png", import.meta.url).href,
+      socials: {
+        instagram: "https://instagram.com/_aryaa_2004",
+        github: "https://github.com/Aryaadeshpande",
+        linkedin: "https://www.linkedin.com/in/arya-d-10575928a",
+      }
+    }
+  ],
+};
 
 
-  export default team;
+export default team;
